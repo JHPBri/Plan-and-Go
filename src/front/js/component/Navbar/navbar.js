61352d06be188d8f395a1dd6153f55cd6d4e7aa7
Lupey1103 <luperon.christian95@gmail.com>
2022-07-08 21:35:44 +0000

    Login Page

--- src/front/js/component/Navbar/navbar.js
+++ src/front/js/component/Navbar/navbar.js
@@ -1,49 +1,46 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "../../../styles/Navbar.css";
-
+import { Login } from "../../pages/Login/Login";
 
 export const Navbar = () => {
-	const [toggleMenu, setToggleMenu] = useState(false)
-	const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
 
-	
-		const toggleNav = () => {
-		setToggleMenu(!toggleMenu)
-		}
+  const toggleNav = () => {
+    setToggleMenu(!toggleMenu);
+  };
 
-		useEffect(() => {
-	
-		const changeWidth = () => {
-			setScreenWidth(window.innerWidth);
-		}
-	
-		window.addEventListener('resize', changeWidth)
-	
-		return () => {
-			window.removeEventListener('resize', changeWidth)
-		}
-	
-		}, [])
+  useEffect(() => {
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+    };
 
-	return(
-		<div className="photoBG">
-			
-			<nav className="navbar">
-				<div className="navbar-container container">
-					
-					<ul className="menu-items">
-						<li><a href="#">Log In</a></li>
-						<li id="sign-up"><a href="#">Sign Up</a></li>
-						
-					</ul>
-					<h1 className="logo">Logo</h1>
-				</div>
-   			</nav>
-			<h1 className="header">Start the memories here.</h1>
-			<h2 className="header2">Find local events happening near you</h2>
-		</div>
-			
-		
-		
-	)
+    window.addEventListener("resize", changeWidth);
+
+    return () => {
+      window.removeEventListener("resize", changeWidth);
+    };
+  }, []);
+
+  return (
+    <div className="photoBG">
+      <nav className="navbar">
+        <div className="navbar-container container">
+          <ul className="menu-items">
+            <li>
+              <a href="login" onClick={Login}>
+                Log In
+              </a>
+            </li>
+            <li id="sign-up" onClick="">
+              <a href="#">Sign Up</a>
+            </li>
+          </ul>
+          <h1 className="logo">Logo</h1>
+        </div>
+      </nav>
+      <h1 className="header">Start the memories here.</h1>
+      <h2 className="header2">Find local events happening near you</h2>
+    </div>
+  );
 };
